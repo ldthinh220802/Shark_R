af67aea364fdc0b44f15c94e97a06a0a8ee52a75
ldthinh220802 <73686232+ldthinh220802@users.noreply.github.com>
2021-05-11 08:45:57 +0700

    Add files via upload

---
+++ ham_tapply.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\DELL\\Downloads")
+# Câu 1
+data = read.table(file = 'Temperature2.txt', header = TRUE)
+tapply(data$Temperature,list(data$Month,data$Year), mean,na.rm = TRUE) #gia tri trung binh
+tapply(data$Temperature,list(data$Month,data$Year), sd,na.rm ? TRUE) #standard deviation
+tapply(data$Temperature,list(data$Month,data$Year), length) #so quan sat
+#Câu 2
+table(data$Year) #so quan sat tren 1 nam
+table(list(data$Year,data$Station)) #so quan sat tren 1 nam/tram
